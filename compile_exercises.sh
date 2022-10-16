66d3eb5a606e0802bc5aed82b05b244f1c40b64b
Hugo Tavares <hm533@cam.ac.uk>
2022-10-16 12:50:58 +0100

    fix script to compile exercises

--- compile_exercises.sh
+++ compile_exercises.sh
@@ -2,8 +2,16 @@
 
 # ===== Compile Exercises ===== #
 
-echo ":::note" > 99-exercises.md
-echo "This page contains all the same exercises found throughout the materials. They are compiled here for convenience." >> 99-exercises.md
-echo ":::" >> 99-exercises.md
+echo '---
+pagetitle: "Sanger HPC"
+output:
+  html_document:
+    toc: false
+---
+' > 99-exercises.md
 
-cat *.md | sed -n '/^:::exercise/,/^:::/p' | sed 's/:::exercise/\n----\n\n:::exercise/g' >> 99-exercises.md
+echo ":::note
+This page contains all the same exercises found throughout the materials. They are compiled here for convenience.
+:::" >> 99-exercises.md
+
+cat 0*.md | sed -n '/^:::exercise/,/^:::/p' | sed 's/:::exercise/\n----\n\n:::exercise/g' >> 99-exercises.md
